0896b71286dbb5e4f63e2801f05332bcdd9ee1ff
TengaIO <samueltenga@gmail.com>
2025-07-05 18:33:39 +0200

    maybe

--- routes/oauth/signin.ts
+++ routes/oauth/signin.ts
@@ -2,7 +2,7 @@ import { Handlers } from "$fresh/server.ts";
 import { oauth2Client } from "../../utils/oauth2.ts";
 
 export const handler: Handlers = {
-  GET(_req) {
-    return Response.redirect(oauth2Client.code.getAuthorizationUri());
+  async GET(_req) {
+    return Response.redirect(await oauth2Client.code.getAuthorizationUri());
   },
 };
